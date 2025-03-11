69903ad698a709bd35bfc28af0b4e5544f8f9478
singhvi1 <singvi182003@gmail.com>
2025-03-11 18:58:44 +0530

    indexing style finished

--- Backend/MAJORPROJECT/models/listing.js
+++ Backend/MAJORPROJECT/models/listing.js
@@ -8,22 +8,17 @@ const listingSchema = new Schema({
   description: {
     type: String,
   },
-  // Image:{
-  //     type:String,
-  // default:"https://images.unsplash.com/photo-1739467372234-2aba33f6b7ee?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  // set:(v)=>v===""?"https://images.unsplash.com/photo-1739467372234-2aba33f6b7ee?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D": v
-  // },
-  Image: {
-    // ✅ Change Image field to an object
-    filename: String,
-    url: {
-      type: String,
-      default:
-        "https://images.unsplash.com/photo-1739467372234-2aba33f6b7ee?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      set: (v) =>
-        v === ""
-          ? "https://images.unsplash.com/photo-1739467372234-2aba33f6b7ee?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          : v,
+
+
+
+  image: {
+    type: {
+      filename: Object,
+      url: String,
+    },
+    default: {
+      filename: "default",
+      url: "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
     },
   },
   price: Number,
